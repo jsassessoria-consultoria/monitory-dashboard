a7a0fa65e58e861f618f1375c1693ff8d00ec3bf
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2023-01-22 22:13:25 -0300

    adiciona setup de testes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
-const { defineConfig } = require('eslint-define-config');
+const eslint = require('eslint-define-config');
 
-module.exports = defineConfig({
+module.exports = eslint.defineConfig({
   root: true,
   extends: ['next/core-web-vitals', 'airbnb-typescript'],
-  overrides: [
-    {
-      parserOptions: {
-        project: './tsconfig.json'
-      },
-      files: ['*.ts', '*.tsx']
-    }
-  ],
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    mocha: true,
+    es6: true
+  },
   rules: {
     'no-console': 'error',
-    "@typescript-eslint/comma-dangle": "off",
-  }
+    '@typescript-eslint/comma-dangle': 'off'
+  },
+  ignorePatterns: ['.eslintrc.js', 'next.config.js', 'vitest.config.ts', 'node_modules/']
 });
