ccb8b067436b3865387a2d978edc4263c6787262
Joao Paulo <oliveirajpufrj@gmail.com>
2023-03-07 18:22:55 -0300

    refactor: update DB model name & functions w/ him

--- src/pages/api/db/users.ts
+++ src/pages/api/db/users.ts
@@ -1,19 +1,22 @@
 import { prisma } from './db';
 
 type UserData = {
-  nome: string;
   email: string;
-  password: string;
+  senha: string;
 };
 
 export async function getUniqueUser(email: string) {
-  return prisma.usuario.findFirstOrThrow({ where: { email } });
+  return prisma.usuario_Web.findFirstOrThrow({ where: { email } });
+}
+
+export async function getOneUser(email: string) {
+  return prisma.usuario_Web.findFirst({ where: { email } });
 }
 
 export async function getAllUser() {
-  return prisma.usuario.findMany({});
+  return prisma.usuario_Web.findMany({});
 }
 
 export async function createUser(data: UserData) {
-  return prisma.usuario.create({ data });
+  return prisma.usuario_Web.create({ data });
 }
