cea4ee1f09b526315c9e4c2e2d4284607d9062ce
João Paulo Oliveira <98192737+OliveiraaJP@users.noreply.github.com>
2023-03-30 20:43:15 -0300

    Update collect.ts (#28)

--- src/lib/prisma/collect.ts
+++ src/lib/prisma/collect.ts
@@ -20,7 +20,7 @@ export async function getOneTempoMonitorado(
 export async function updateOneTempoMonitorado(id: string) {
   return prisma.tempoMonitorado.update({
     where: { id },
-    data: { tempo: { increment: 10 } }
+    data: { tempo: { increment: 60000 } }
   });
 }
 
@@ -63,41 +63,9 @@ export async function createOneTempoMonitorado(
 ) {
   try {
     return await prisma.tempoMonitorado.create({
-      data: { date, software, dispositivo_id, tempo: 10 }
+      data: { date, software, dispositivo_id, tempo: 60000 }
     });
   } catch (error: any) {
     throw new Error(error);
   }
 }
-
-/* export async function createOneTempoMonitorado(
-  date: string,
-  software: string,
-  dispositivo_id: string
-) {
-  return prisma.tempoMonitorado.create({
-    data: { date, software, dispositivo_id, tempo: 10 }
-  });
-} */
-
-/* export async function createOneTempoMonitoradoAndLocation(
-  date: string,
-  software: string,
-  dispositivo_id: string,
-  lat: string | null = null,
-  long: string | null = null,
-  isAccuracy: boolean | null = null
-) {
-  try {
-    return await prisma.$transaction(async tx => {
-      await tx.tempoMonitorado.create({
-        data: { date, software, dispositivo_id, tempo: 10 }
-      });
-      await tx.localizacao.create({
-        data: { lat, long, isAccuracy, dispositivo_id }
-      });
-    });
-  } catch (error: any) {
-    throw new Error(error);
-  }
-} */
